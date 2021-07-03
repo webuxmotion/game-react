de67eaaf67a8fa71c4cceb2f3a0365639aae4458
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-07-03 17:35:35 +0300

    Add GameMachine

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,8 +1,9 @@
-import { Meta } from '@storybook/react/types-6-0';
-import { Button } from '.';
+import React from "react";
+import { Button } from ".";
+import { Meta } from "@storybook/react/types-6-0";
 
 export default {
-  title: `Small Components/Button`
-} as Meta
+  title: `Small Components/Button`,
+} as Meta;
 
-export const Index = () => <Button>Start Game</Button>
+export const Index = () => <Button>Start Game</Button>;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,10 @@
-import { FC } from 'react';
-import { Button as StyledButton } from './styles';
+import styled from "styled-components";
+import { space } from "../../styles/space";
 
-export const Button: FC = ({ children, ...props }) => {
-  return (
-    <StyledButton {...props}>{children}</StyledButton>
-  )
-}
+export const Button = styled.button`
+    padding: ${space(3)} ${space(6)};
+    border: 2px solid ${(props) => props.theme.color.white};
+
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+`;
