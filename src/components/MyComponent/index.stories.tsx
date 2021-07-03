40b344002500ef8b56f47bb4045ca748659c86cb
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-07-03 14:25:37 +0300

    Initial commit

---
+++ src/components/MyComponent/index.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta } from '@storybook/react/types-6-0';
+import { MyComponent } from '.';
+
+export default {
+  title: `MyComponent`
+} as Meta
+
+export const Index = () => <MyComponent />
